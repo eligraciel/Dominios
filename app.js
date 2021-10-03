b32d80718b6b369cb06afe670c5e2511a0109857
eligraciel <eliana.bravo@gmail.com>
2021-10-03 16:25:09 +0000

    Code generador de dominios

---
+++ app.js
@@ -0,0 +1,23 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+var arrExt = ['.net', '.us', '.io', '.es'];
+let pronombre;
+let adjetivo;
+let nombre;
+
+
+for (i=0; i<pronoun.length; i++) {
+    pronombre = pronoun[i];
+    for (j=0; j<adj.length; j++) {
+        adjetivo = adj[j];
+        for (n=0; n<noun.length; n++) {
+            let ext = Math.floor(Math.random()*4);
+            nombre = noun[n];
+            console.log(pronombre + adjetivo + nombre + arrExt[ext]);
+        }
+    }
+
+}
+
+
